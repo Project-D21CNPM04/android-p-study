af97e1d604784d322017dd004aa9fe5da4c69a89
linhmyx1512 <linhmyx2003@gmail.com>
2025-04-20 04:31:08 +0700

    [HomeActivity]: done UI home screen

--- android/app/src/main/java/com/example/pstudy/view/home/HomeActivity.kt
+++ android/app/src/main/java/com/example/pstudy/view/home/HomeActivity.kt
@@ -3,20 +3,84 @@ package com.example.pstudy.view.home
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.base.ui.base.BindingActivity
 import com.example.base.utils.startActivityAsRoot
+import com.example.pstudy.R
 import com.example.pstudy.data.FirebaseAuthHelper
 import com.example.pstudy.databinding.ActivityHomeBinding
+import com.example.pstudy.databinding.ItemHomeTabLayoutBinding
+import com.example.pstudy.view.home.adapter.PagerAdapter
+import com.example.pstudy.view.home.fragment.AllFragment
+import com.example.pstudy.view.home.viewmodel.HomeViewModel
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>() {
     override fun inflateBinding(layoutInflater: LayoutInflater) =
         ActivityHomeBinding.inflate(layoutInflater)
 
+    private val viewModel by viewModels<HomeViewModel>()
+    private val pagerAdapter by lazy { PagerAdapter(this) }
+
+    override fun getStatusBarColor() = R.color.black
+
     override fun updateUI(savedInstanceState: Bundle?) {
+        initView()
+        setupPager()
+        handleOnClick()
+    }
+
+    private fun initView() {
+        binding.tvGreeting.text = buildString {
+            append(getString(R.string.home_greeting))
+            append(" ")
+            append(FirebaseAuthHelper.getCurrentUserName())
+        }
+    }
+
+    private fun setupPager() {
+        pagerAdapter.apply {
+            addFragment(AllFragment())
+            addFragment(AllFragment())
+            addFragment(AllFragment())
+            binding.viewPager.adapter = this
+        }
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            val tabBinding = ItemHomeTabLayoutBinding.inflate(LayoutInflater.from(this))
+            tabBinding.tvTitle.text = viewModel.getItemTab(this, position)
+            tab.customView = tabBinding.root
+        }.attach()
+
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) =
+                updateTabStyle(tab, R.color.primary_blue)
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) =
+                updateTabStyle(tab, android.R.color.transparent)
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {} // No action needed
+        })
+
+        updateTabStyle(binding.tabLayout.getTabAt(0), R.color.primary_blue)
+    }
+
+    private fun updateTabStyle(tab: TabLayout.Tab?, colorRes: Int) {
+        tab?.customView?.findViewById<TextView>(R.id.tvTitle)?.apply {
+            backgroundTintList = ContextCompat.getColorStateList(context, colorRes)
+            isSingleLine = true
+        }
+    }
+
+    private fun handleOnClick() {
         with(binding) {
-            tvName.text = FirebaseAuthHelper.getCurrentUserName()
-            tvMail.text = FirebaseAuthHelper.getCurrentUserEmail()
-            tvUid.text = FirebaseAuthHelper.getCurrentUserUid()
+            icAdd.setOnClickListener {
+                addFeature.isVisible = !addFeature.isVisible
+            }
         }
     }
 
