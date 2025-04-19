04e5864188079b4d728d0bb6bca379596122b894
harrybui03 <mrflooo.1230@gmail.com>
2025-04-19 23:31:15 +0700

    Handle structure result

---
+++ android/app/src/main/java/com/example/pstudy/view/result/fragment/QuizsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.pstudy.view.result.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.example.base.ui.base.BindingFragmentLazyPager
+import com.example.pstudy.databinding.FragmentQuizsBinding
+import com.example.pstudy.view.result.ResultViewModel
+
+class QuizsFragment : BindingFragmentLazyPager<FragmentQuizsBinding>() {
+
+    private val viewModel: ResultViewModel by activityViewModels()
+
+    override fun inflateBinding(inflater: LayoutInflater): FragmentQuizsBinding {
+        return FragmentQuizsBinding.inflate(inflater)
+    }
+
+    override fun updateUI() {
+        println("QuizsFragment updateUI called")
+        // TODO: Observe viewModel LiveData here
+        // Example: viewModel.quizsData.observe(viewLifecycleOwner) { ... }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initial setup using the shared viewModel if needed
+    }
+
+    companion object {
+        fun newInstance() = QuizsFragment()
+    }
+}
