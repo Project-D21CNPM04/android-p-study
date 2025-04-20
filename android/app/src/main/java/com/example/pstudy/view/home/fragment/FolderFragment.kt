f330ce050f286962a50ba2c65ac47352fe51eb97
linhmyx1512 <linhmyx2003@gmail.com>
2025-04-20 22:36:19 +0700

    enhance home UI: add empty state layout and update fragments

---
+++ android/app/src/main/java/com/example/pstudy/view/home/fragment/FolderFragment.kt
@@ -0,0 +1,17 @@
+package com.example.pstudy.view.home.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.example.base.ui.base.BindingFragment
+import com.example.pstudy.databinding.FragmentFolderBinding
+
+class FolderFragment : BindingFragment<FragmentFolderBinding>() {
+    override fun inflateBinding(inflater: LayoutInflater) =
+        FragmentFolderBinding.inflate(inflater)
+
+    override fun updateUI(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
