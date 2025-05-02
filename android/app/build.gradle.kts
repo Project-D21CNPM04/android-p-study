5a0079cc418a27a78edd58e0f8dd015cf9a88279
linhmyx1512 <linhmyx2003@gmail.com>
2025-05-02 16:38:19 +0700

    Add retrofit dependency and client implementation for remote data access

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
 }
